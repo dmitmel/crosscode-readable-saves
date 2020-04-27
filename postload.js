21292c68e531a2dfd9a996c261ab7122a480aa38
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-04-27 20:49:27 +0300

    complete rewrite, split save data into different files (because not everyone uses vim which can open large files)

--- postload.js
+++ postload.js
@@ -5,125 +5,147 @@ if (ig.platform !== ig.PLATFORM_TYPES.DESKTOP) {
 ig.module('readable-saves')
   .requires('impact.feature.storage.storage')
   .defines(() => {
-    const fs = require('fs');
+    const fs = require('fs').promises;
     const path = require('path');
 
     function serializePretty(data) {
       return JSON.stringify(data, null, 2);
     }
 
-    ig.StorageDataSimple = ig.StorageData.extend({
-      _saveToFile() {
-        // save immediately, skip renaming backup files
-        this._doIoStep({ save: true });
+    async function mkdirIfNotExists(dir, options) {
+      try {
+        await fs.mkdir(dir, options);
+      } catch (err) {
+        if (err.code !== 'EEXIST') throw err;
+      }
+    }
+
+    function countDigits(n) {
+      n = Math.abs(n);
+      let result = 1;
+      while (n >= 10) {
+        n /= 10;
+        result++;
+      }
+      return result;
+    }
+
+    // TODO: explain why I don't use backups
+
+    ig.StorageDataReadable = ig.Class.extend({
+      path: null,
+      saveInProgress: false,
+      queuedSaveData: null,
+
+      init(p) {
+        this.path = p;
       },
 
-      // Dammit, RFG, why don't you just print error stack traces by default?
-      // That's what the console is for.
-      _loadResponse(err, ...args) {
-        if (err != null && err.code !== 'ENOENT') {
-          console.error(err);
-          this._loadCallback(this.cacheType, this.path, false);
-          // yes, this second throw does look weird, but _loadCallback itself
-          // indirectly throws an error if the last flag is `false`
-          throw err;
-        }
-        return this.parent(err, ...args);
+      save(data) {
+        if (!this.saveInProgress) this._writeToDisk(data);
+        else this.queuedSaveData = data;
       },
 
-      _saveResponse(err, ...args) {
-        // Where does this error usually end up? Correct: it isn't shown to the
-        // user, but the user definitely has the right to know what happens to
-        // their save files, hence the `console.error`
-        if (err != null) {
-          // note that here I don't crash the game with `ig.system.error`
-          // because user might still have the chance to make their savefile
-          // readable, with `chmod` or `chown` for example
-          console.error(err);
-          // reset the error so that data of this `ig.StorageData` instance
-          // isn't written into `localStorage` (there is no point), but
-          // subsequent writes to the disk are still executed
-          err = null;
-        }
-        return this.parent(err, ...args);
-      },
+      async _writeToDisk(data) {
+        this.saveInProgress = true;
 
-      _getSaveFilePathList() {
-        let pathList = [];
-
-        // On Windows `nw.App.dataPath` is `%LOCALAPPDATA%\CrossCode\User Data\Default`,
-        // yet the game writes the savegame to `%LOCALAPPDATA%\CrossCode` when
-        // possible, so I reproduce this behavior. Notice that this
-        // implementation IS BROKEN when %LOCALAPPDATA% contains the
-        // `\User Data\Default` substring, but eh, whatever, this is the exact
-        // piece of code the stock game uses.
-        let { dataPath } = nw.App;
-        let userDataIndex = dataPath.indexOf('\\User Data\\Default');
-        if (userDataIndex >= 0) {
-          let dataPathRoot = dataPath.slice(0, userDataIndex);
-          pathList.push(path.join(dataPathRoot, this.path));
-        }
-        pathList.push(path.join(dataPath, this.path));
-
-        return pathList;
-      },
-
-      _loadStorageFromData(text) {
         try {
-          this.data = JSON.parse(text);
+          let rootDir = path.join(
+            this.constructor.GAME_DATA_DIRECTORIES[0],
+            'cc-readable-save',
+          );
+
+          // TODO: explain file modes
+
+          // first of all, I create the directory structure in correct order
+          await mkdirIfNotExists(rootDir);
+          let slotsDir = path.join(rootDir, 'slots');
+          await mkdirIfNotExists(slotsDir);
+
+          let slotFileDigits = Math.max(2, countDigits(data.slots.length));
+
+          // now that all directories have been created, I can fire up all
+          // actual file writes in parallel FOR MASSIVE PERFORMANCE BOOST
+          await Promise.all([
+            ...data.slots.map((slotData, index) => {
+              let indexStr = index.toString(10).padStart(slotFileDigits, '0');
+              let filename = `${indexStr}.json`;
+              return fs.writeFile(path.join(slotsDir, filename), slotData);
+            }),
+            fs.writeFile(path.join(rootDir, 'autoSlot.json'), data.autoSlot),
+            fs.writeFile(path.join(rootDir, 'globals.json'), data.globals),
+            fs.writeFile(path.join(rootDir, 'misc.json'), data.misc),
+          ]);
         } catch (err) {
-          err.message = `Failed to parse ${this.path}: ${err.message}`;
-          ig.system.error(err);
-          return false;
+          // note that here I don't crash the game with `ig.system.error`
+          // because user might still have the chance of fixing their
+          // filesystem and making their savefile writable, with `chmod` or
+          // `chown` for example
+          console.error(err);
         }
 
-        // skip the decryption and JSON parsing in the original implementation
-        // by passing an empty string because it has an if which checks if
-        // `text` is truthy
-        this.parent('');
+        this._onWriteFinished();
+      },
+
+      _onWriteFinished() {
+        if (this.queuedSaveData != null) {
+          let data = this.queuedSaveData;
+          this.queuedSaveData = null;
+          this._writeToDisk(data);
+        } else {
+          this.saveInProgress = false;
+        }
       },
     });
 
+    {
+      let pathList = [];
+
+      // On Windows `nw.App.dataPath` is `%LOCALAPPDATA%\CrossCode\User Data\Default`,
+      // yet the game writes the savegame to `%LOCALAPPDATA%\CrossCode` when
+      // possible, so I reproduce this behavior. Notice that this implementation
+      // IS BROKEN when %LOCALAPPDATA% contains the `\User Data\Default`
+      // substring, but eh, whatever, this is the exact piece of code the stock
+      // game uses.
+      let { dataPath } = nw.App;
+      let userDataIndex = dataPath.indexOf('\\User Data\\Default');
+      if (userDataIndex >= 0) pathList.push(dataPath.slice(0, userDataIndex));
+      pathList.push(dataPath);
+
+      ig.StorageDataReadable.GAME_DATA_DIRECTORIES = pathList;
+    }
+
     ig.Storage.inject({
-      readableData: new ig.StorageDataSimple('cc-readable-save.json'),
+      readableData: new ig.StorageDataReadable('cc-readable-save.json'),
 
-      init(...args) {
-        if (this.readableData.data != null) {
-          let oldData = this.data.data;
-          this.data.data = this.readableData.data;
-          this.parent(...args);
-          this.data.data = oldData;
-        } else {
-          this.parent(...args);
+      _saveToStorage() {
+        let globals = {};
+        for (let listener of this.listeners) {
+          if (listener.onStorageGlobalSave != null) {
+            listener.onStorageGlobalSave(globals);
+          }
         }
-      },
 
-      _saveToStorage(...args) {
-        let globals = null;
-
-        // usually globals listeners are used for constructing the `globals`
-        // object, but here I take advantage of them and use a listener to read
-        // the state of globals instead.
-        let fakeGlobalsListener = {
-          onStorageGlobalSave: (globals2) => {
-            globals = globals2;
-          },
+        let savesData = {
+          slots: this.slots.map((slot) => slot.data),
+          autoSlot: this.autoSlot != null ? this.autoSlot.data : null,
+          globals,
+          lastSlot: this.lastUsedSlot,
         };
+        // TODO: explain that replacing the original save format is perfectly
+        // backward-compatible
+        this.data.save(JSON.stringify(savesData));
 
-        this.listeners.push(fakeGlobalsListener);
-        let result = this.parent(...args);
-        this.listeners.pop();
+        // TODO: consider using https://github.com/ibmruntimes/yieldable-json
+        // for faster serialization
+        this.readableData.save({
+          slots: savesData.slots.map((slot) => serializePretty(slot)),
+          autoSlot: serializePretty(savesData.autoSlot),
+          globals: serializePretty(savesData.globals),
+          misc: serializePretty({ lastSlot: savesData.lastSlot }),
+        });
 
-        this.readableData.save(
-          serializePretty({
-            slots: this.slots.map((slot) => slot.data),
-            autoSlot: this.autoSlot.data,
-            globals,
-            lastSlot: this.lastUsedSlot,
-          }),
-        );
-
-        return result;
+        return savesData;
       },
     });
   });
